750de3a61e7eb7ac6bdf0241b7e617983aee80b5
michael <michaellaidlawscott96@gmail.com>
2021-07-26 21:55:16 +0100

    renaming

--- libs/celestial-web/home/jest.config.js
+++ libs/celestial-web/home/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  displayName: 'celestial-home',
+  displayName: 'celestial-web-home',
   preset: '../../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/celestial/home',
+  coverageDirectory: '../../../coverage/libs/celestial-web/home',
 };
